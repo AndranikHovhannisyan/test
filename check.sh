9af62f4483e0c7861b1d8ade820cfa79d7f4dac2
Andranik <andranik.hovhan@gmail.com>
2020-09-11 13:30:43 +0400

    change script

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 set -e
 echo -n "Checking if there are uncommited changes... "
-trap 'echo -e "\033[0;31mFAILED\033[0m"' ERR
+trap 'echo -e "\033[0;31mCHANGED\033[0m"' ERR
 git diff-index --quiet HEAD --
 trap - ERR
-echo -e "\033[0;32mAll set!\033[0m"
+echo -e "\033[0;32mUNCHANGED\033[0m"
